d05c0c426819df43f7bd9adb8e2db5b2790042e0
SeckinKurtalan <71966684+SeckinKurtalan@users.noreply.github.com>
2024-05-15 15:23:38 +0300

    a

---
+++ Online-Education-System/onlineeducationsystem/src/main/java/com/egeuniversity/onlineeducationsystem/Service/abstracts/CourseService.java
@@ -0,0 +1,15 @@
+package com.egeuniversity.onlineeducationsystem.Service.abstracts;
+
+import com.egeuniversity.onlineeducationsystem.data.Course;
+import com.egeuniversity.onlineeducationsystem.dto.CourseSearchDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+    Course addCourse(Course course); // Add a new course
+    Course updateCourse(String id, Course courseDetails);
+    Course getCourseById(String id);
+    Page<Course> getAllCourses(CourseSearchDTO dto) throws Exception;
+    void deleteCourse(String id);
+    Course addUserToCourse(String courseId, String userId);
+}
---
+++ Online-Education-System/onlineeducationsystem/src/main/java/com/egeuniversity/onlineeducationsystem/Service/abstracts/UserService.java
@@ -0,0 +1,29 @@
+package com.egeuniversity.onlineeducationsystem.Service.abstracts;
+
+import com.egeuniversity.onlineeducationsystem.dto.UserDTO;
+import com.egeuniversity.onlineeducationsystem.data.User;
+import com.egeuniversity.onlineeducationsystem.dto.UserSearchDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface UserService {
+
+    User addUser(User user);
+
+    void removeUser(String userId);
+
+    User getUser(String userId);
+
+    User updateUser(String id, UserDTO dto);
+
+    Page<User> listUsers(UserSearchDTO dto) throws Exception;
+
+    String handleFileUpload(String id, MultipartFile file) throws IOException;
+
+    User signup(UserDTO userDTO);
+
+    User login(String email, String password);
+}
